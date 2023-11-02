235867a31f87fe281145fdc5bbba3a58a130450b
Dipankar Arora <dipankar.a511@gmail.com>
2023-11-02 00:55:14 -0600

    backend msg change check again deploy

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 app.get('/', (req, res) =>{
     res.json(
         {
-            mesg:'Food Ordering - Backend check again'
+            mesg:'Food Ordering - Backend check again deploy'
         })
 })
 
